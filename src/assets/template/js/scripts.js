f4cf7faf16681087b9e1467b93c5d024136b9171
dim-2g <dim-2g@yandex.ru>
2017-10-26 02:07:36 +0300

    по главной

---
+++ src/assets/template/js/scripts.js
@@ -0,0 +1,58 @@
+$(function() {
+    stepsSlider = function(){
+        $('.process__list').not('.slick-initialized').slick({
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: false,
+            centerPadding: '0px',
+            centerMode: true,
+            focusOnSelect: true,
+            adaptiveHeight: true,
+            infinite: false,
+            responsive: [
+                {
+                    breakpoint: 9999,
+                    settings: "unslick"
+                },
+                {
+                    breakpoint: 1000,
+                    settings: {
+                        slidesToShow: 1,
+                        arrows: false,
+                        dots: false,
+                    }
+                }
+            ]
+        });
+    }
+    stepsSlider();
+
+    sertificatySlider = function(){
+        $('.sertificaty__list').not('.slick-initialized').slick({
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: true,
+            focusOnSelect: true,
+            adaptiveHeight: true,
+            infinite: true,
+            responsive: [
+                {
+                    breakpoint: 1000,
+                    settings: {
+                        slidesToShow: 1,
+                        dots: false,
+                    }
+                }
+            ]
+        });
+    }
+    sertificatySlider();
+
+});
+$(window).resize(function(){
+    var width = $(window).width();
+
+    stepsSlider();
+});
